83b331860fe40af09b8123e9bfc1775a7045de9c
STEPin105113 <69115366+STEPin105113@users.noreply.github.com>
2021-01-09 09:58:35 +0530

    Update calc.c

--- calc.c
+++ calc.c
@@ -32,7 +32,14 @@ long int cube(long int a)
 {
 	return a*a*a;
 }
+float cube_root(double a)
+{
+   float i, precision = 0.000001;
+   for(i = 1; (i*i*i) <= a; ++i);        
+   for(--i; (i*i*i) < a; i += precision);  
+   return i;
 
+}
 
 float square_root(double a)
 {
